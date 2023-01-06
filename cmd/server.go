c05bd5bf7c4205c01d29c06cd100ec761616c639
Alejandro Falkowski <alexrfalkowski@gmail.com>
2023-01-06 18:51:08 +0100

    feat: update github.com/alexfalkowski/go-service to v1.78.0 (#79)

--- cmd/server.go
+++ cmd/server.go
@@ -6,8 +6,10 @@ import (
 	"github.com/alexfalkowski/bezeichner/server/health"
 	v1 "github.com/alexfalkowski/bezeichner/server/v1"
 	"github.com/alexfalkowski/go-service/cache"
+	"github.com/alexfalkowski/go-service/cmd"
 	"github.com/alexfalkowski/go-service/database/sql"
 	"github.com/alexfalkowski/go-service/logger"
+	"github.com/alexfalkowski/go-service/marshaller"
 	"github.com/alexfalkowski/go-service/metrics"
 	"github.com/alexfalkowski/go-service/transport"
 	"go.uber.org/fx"
@@ -15,7 +17,8 @@ import (
 
 // ServerOptions for cmd.
 var ServerOptions = []fx.Option{
-	fx.NopLogger, fx.Provide(NewVersion), config.Module, health.Module,
+	fx.NopLogger, marshaller.Module, cmd.Module,
+	fx.Provide(NewVersion), config.Module, health.Module,
 	logger.ZapModule, metrics.PrometheusModule, transport.Module,
 	sql.PostgreSQLModule, sql.PostgreSQLOpentracingModule,
 	cache.RedisModule, cache.RedisOpentracingModule,
