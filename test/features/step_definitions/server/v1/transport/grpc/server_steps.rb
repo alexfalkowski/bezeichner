f63b8668550c4d0f2b6640d3cabedf91e086cdf9
Alejandro Falkowski <alexrfalkowski@gmail.com>
2023-07-19 21:24:42 +0200

    test(ruby): upgrade to version 3.2.2

--- test/features/step_definitions/server/v1/transport/grpc/server_steps.rb
+++ test/features/step_definitions/server/v1/transport/grpc/server_steps.rb
@@ -6,7 +6,7 @@ When('I request to generate identifiers with gRPC:') do |table|
   metadata = { 'request-id' => @request_id, 'ua' => Bezeichner.server_config['transport']['grpc']['user_agent'] }
 
   request = Bezeichner::V1::GenerateIdentifiersRequest.new(application: rows['application'], count: rows['count'].to_i)
-  @response = Bezeichner::V1.server_grpc.generate_identifiers(request, { metadata: metadata })
+  @response = Bezeichner::V1.server_grpc.generate_identifiers(request, { metadata: })
 rescue StandardError => e
   @response = e
 end
@@ -17,7 +17,7 @@ When('I request to map identifiers with gRPC:') do |table|
   metadata = { 'request-id' => @request_id, 'ua' => Bezeichner.server_config['transport']['grpc']['user_agent'] }
 
   request = Bezeichner::V1::MapIdentifiersRequest.new(ids: rows['request'].split(','))
-  @response = Bezeichner::V1.server_grpc.map_identifiers(request, { metadata: metadata })
+  @response = Bezeichner::V1.server_grpc.map_identifiers(request, { metadata: })
 rescue StandardError => e
   @response = e
 end
