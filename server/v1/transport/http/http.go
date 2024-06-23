ab586bdf934c8308f19a12b8980778243b18de43
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-06-23 18:42:38 +0200

    fix(http): remove package alias (#633)

--- server/v1/transport/http/http.go
+++ server/v1/transport/http/http.go
@@ -3,18 +3,17 @@ package http
 import (
 	"github.com/alexfalkowski/bezeichner/server/service"
 	"github.com/alexfalkowski/go-service/marshaller"
-	nh "github.com/alexfalkowski/go-service/net/http"
+	"github.com/alexfalkowski/go-service/net/http"
 	"go.uber.org/fx"
 )
 
 type (
-
 	// RegisterParams for HTTP.
 	RegisterParams struct {
 		fx.In
 
 		Marshaller *marshaller.Map
-		Mux        nh.ServeMux
+		Mux        http.ServeMux
 		Service    *service.Service
 	}
 
@@ -33,12 +32,12 @@ type (
 func Register(params RegisterParams) error {
 	s := &Server{service: params.Service}
 
-	gih := nh.NewHandler[GenerateIdentifiersRequest](params.Mux, params.Marshaller, &generateErrorer{})
+	gih := http.NewHandler[GenerateIdentifiersRequest](params.Mux, params.Marshaller, &generateErrorer{})
 	if err := gih.Handle("POST", "/v1/generate", s.GenerateIdentifiers); err != nil {
 		return err
 	}
 
-	mih := nh.NewHandler[MapIdentifiersRequest](params.Mux, params.Marshaller, &mapErrorer{})
+	mih := http.NewHandler[MapIdentifiersRequest](params.Mux, params.Marshaller, &mapErrorer{})
 	if err := mih.Handle("POST", "/v1/map", s.MapIdentifiers); err != nil {
 		return err
 	}
