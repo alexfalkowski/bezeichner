de3fbc43d235c611fdd0372020fdc9dbc566885a
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-06-25 08:12:59 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.221.0 (#636)

--- server/v1/transport/http/http.go
+++ server/v1/transport/http/http.go
@@ -2,21 +2,10 @@ package http
 
 import (
 	"github.com/alexfalkowski/bezeichner/server/service"
-	"github.com/alexfalkowski/go-service/marshaller"
 	"github.com/alexfalkowski/go-service/net/http"
-	"go.uber.org/fx"
 )
 
 type (
-	// RegisterParams for HTTP.
-	RegisterParams struct {
-		fx.In
-
-		Marshaller *marshaller.Map
-		Mux        http.ServeMux
-		Service    *service.Service
-	}
-
 	// Server for HTTP.
 	Server struct {
 		service *service.Service
@@ -29,18 +18,9 @@ type (
 )
 
 // Register for HTTP.
-func Register(params RegisterParams) error {
-	s := &Server{service: params.Service}
+func Register(service *service.Service) {
+	s := &Server{service: service}
 
-	gih := http.NewHandler[GenerateIdentifiersRequest](params.Mux, params.Marshaller, &generateErrorer{})
-	if err := gih.Handle("POST", "/v1/generate", s.GenerateIdentifiers); err != nil {
-		return err
-	}
-
-	mih := http.NewHandler[MapIdentifiersRequest](params.Mux, params.Marshaller, &mapErrorer{})
-	if err := mih.Handle("POST", "/v1/map", s.MapIdentifiers); err != nil {
-		return err
-	}
-
-	return nil
+	http.Handler("POST /v1/generate", &generateErrorer{}, s.GenerateIdentifiers)
+	http.Handler("POST /v1/map", &mapErrorer{}, s.MapIdentifiers)
 }
