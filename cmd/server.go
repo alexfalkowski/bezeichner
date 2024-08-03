afc09d2782a4bed2f10ed1308bfeafa1667e14bd
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-08-03 10:33:53 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.267.0 (#702)

--- cmd/server.go
+++ cmd/server.go
@@ -20,7 +20,7 @@ import (
 
 // ServerOptions for cmd.
 var ServerOptions = []fx.Option{
-	runtime.Module, debug.Module, feature.NoopModule,
+	runtime.Module, debug.Module, feature.Module,
 	compress.Module, encoding.Module,
 	transport.Module, health.Module,
 	telemetry.Module, metrics.Module,
