407530bee56641feba7d93d643120daf9899e7e1
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-01-23 05:48:59 +0100

    feat(api): simplify expressions (#935)

--- api/v1/transport/grpc/generate.go
+++ api/v1/transport/grpc/generate.go
@@ -10,16 +10,10 @@ import (
 // GetIdentifiers for gRPC.
 func (s *Server) GenerateIdentifiers(ctx context.Context, req *v1.GenerateIdentifiersRequest) (*v1.GenerateIdentifiersResponse, error) {
 	resp := &v1.GenerateIdentifiersResponse{}
-
 	ids, err := s.service.Generate(ctx, req.GetApplication(), req.GetCount())
-	if err != nil {
-		resp.Meta = meta.CamelStrings(ctx, "")
 
-		return resp, s.error(err)
-	}
-
-	resp.Ids = ids
 	resp.Meta = meta.CamelStrings(ctx, "")
+	resp.Ids = ids
 
-	return resp, nil
+	return resp, s.error(err)
 }
--- api/v1/transport/http/generate.go
+++ api/v1/transport/http/generate.go
@@ -3,7 +3,6 @@ package http
 import (
 	"context"
 
-	"github.com/alexfalkowski/bezeichner/api/ids"
 	"github.com/alexfalkowski/go-service/meta"
 )
 
@@ -19,24 +18,15 @@ type (
 		Meta map[string]string `json:"meta,omitempty"`
 		IDs  []string          `json:"ids,omitempty"`
 	}
-
-	generateHandler struct {
-		service *ids.Identifier
-	}
 )
 
-func (h *generateHandler) Generate(ctx context.Context, req *GenerateIdentifiersRequest) (*GenerateIdentifiersResponse, error) {
+// GenerateIdentifiers for HTTP.
+func (h *Handler) GenerateIdentifiers(ctx context.Context, req *GenerateIdentifiersRequest) (*GenerateIdentifiersResponse, error) {
 	resp := &GenerateIdentifiersResponse{}
-
 	ids, err := h.service.Generate(ctx, req.Application, req.Count)
-	if err != nil {
-		resp.Meta = meta.CamelStrings(ctx, "")
 
-		return resp, handleError(err)
-	}
-
-	resp.IDs = ids
 	resp.Meta = meta.CamelStrings(ctx, "")
+	resp.IDs = ids
 
-	return resp, nil
+	return resp, h.error(err)
 }
