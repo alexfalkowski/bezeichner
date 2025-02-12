177622afaa01e3c86d1dbb580c00d4cd612bbde4
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-02-12 19:37:39 +0100

    feat(cmd): move to use register (#993)

--- internal/cmd/server.go
+++ internal/cmd/server.go
@@ -6,19 +6,21 @@ import (
 	"github.com/alexfalkowski/bezeichner/internal/generator"
 	"github.com/alexfalkowski/bezeichner/internal/health"
 	"github.com/alexfalkowski/go-service/cache"
+	"github.com/alexfalkowski/go-service/cmd"
 	"github.com/alexfalkowski/go-service/database/sql"
 	"github.com/alexfalkowski/go-service/debug"
 	"github.com/alexfalkowski/go-service/feature"
 	"github.com/alexfalkowski/go-service/module"
 	"github.com/alexfalkowski/go-service/telemetry"
 	"github.com/alexfalkowski/go-service/transport"
-	"go.uber.org/fx"
 )
 
-// ServerOptions for cmd.
-var ServerOptions = []fx.Option{
-	module.Module, feature.Module, debug.Module,
-	transport.Module, telemetry.Module,
-	cache.Module, sql.Module, health.Module,
-	generator.Module, v1.Module, config.Module, Module,
+// RegisterServer for cmd.
+func RegisterServer(command *cmd.Command) {
+	command.AddServer("server", "Start bezeichner server",
+		module.Module, feature.Module, debug.Module,
+		transport.Module, telemetry.Module,
+		cache.Module, sql.Module, health.Module,
+		generator.Module, v1.Module, config.Module, Module,
+	)
 }
