9d68cebf80b83ff7baedf2b65c62d7543b39cad2
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-04-11 20:46:21 +0200

    feat(api): use grpc full method name (#1126)

--- test/lib/bezeichner/v1/http.rb
+++ test/lib/bezeichner/v1/http.rb
@@ -4,11 +4,11 @@ module Bezeichner
   module V1
     class HTTP < Nonnative::HTTPClient
       def generate(application, count, opts = {})
-        post('v1/generate', { application:, count: }.to_json, opts)
+        post('/bezeichner.v1.Service/GenerateIdentifiers', { application:, count: }.to_json, opts)
       end
 
       def map(ids, opts = {})
-        post('v1/map', { ids: }.to_json, opts)
+        post('/bezeichner.v1.Service/MapIdentifiers', { ids: }.to_json, opts)
       end
     end
   end
