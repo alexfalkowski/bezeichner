a2c3b45b0fc6679d6021c92621d544ff9f3581af
Alex Falkowski <alexrfalkowski@gmail.com>
2025-04-11 20:42:47 +0200

    feat(api): use grpc full method name

--- internal/api/v1/transport/http/http.go
+++ internal/api/v1/transport/http/http.go
@@ -1,12 +1,13 @@
 package http
 
 import (
+	v1 "github.com/alexfalkowski/bezeichner/api/bezeichner/v1"
 	"github.com/alexfalkowski/bezeichner/internal/api/v1/transport/grpc"
 	"github.com/alexfalkowski/go-service/net/http/rpc"
 )
 
 // Register for HTTP.
 func Register(server *grpc.Server) {
-	rpc.Route("/v1/generate", server.GenerateIdentifiers)
-	rpc.Route("/v1/map", server.MapIdentifiers)
+	rpc.Route(v1.Service_GenerateIdentifiers_FullMethodName, server.GenerateIdentifiers)
+	rpc.Route(v1.Service_MapIdentifiers_FullMethodName, server.MapIdentifiers)
 }
