47446e3719e133effb0b4a14a0657713813cd3ba
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-05-28 15:02:23 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service/v2 to v2.18.0 (#1238)
    https://github.com/alexfalkowski/go-service/pull/1641

--- internal/cmd/server.go
+++ internal/cmd/server.go
@@ -1,27 +1,10 @@
 package cmd
 
-import (
-	v1 "github.com/alexfalkowski/bezeichner/internal/api/v1"
-	"github.com/alexfalkowski/bezeichner/internal/config"
-	"github.com/alexfalkowski/bezeichner/internal/generator"
-	"github.com/alexfalkowski/bezeichner/internal/health"
-	"github.com/alexfalkowski/go-service/v2/cache"
-	"github.com/alexfalkowski/go-service/v2/cli"
-	"github.com/alexfalkowski/go-service/v2/database/sql"
-	"github.com/alexfalkowski/go-service/v2/debug"
-	"github.com/alexfalkowski/go-service/v2/feature"
-	"github.com/alexfalkowski/go-service/v2/module"
-	"github.com/alexfalkowski/go-service/v2/telemetry"
-	"github.com/alexfalkowski/go-service/v2/transport"
-)
+import "github.com/alexfalkowski/go-service/v2/cli"
 
 // RegisterServer for cmd.
 func RegisterServer(command cli.Commander) {
-	cmd := command.AddServer("server", "Start bezeichner server",
-		module.Module, feature.Module, debug.Module,
-		transport.Module, telemetry.Module,
-		cache.Module, sql.Module, health.Module,
-		generator.Module, v1.Module, config.Module, cli.Module,
-	)
+	cmd := command.AddServer("server", "Start bezeichner server", Module)
+
 	cmd.AddInput("")
 }
