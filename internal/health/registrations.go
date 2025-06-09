c01f2a622f8041dbdd82a500629ddbb10f02bc10
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-06-09 08:35:48 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service/v2 to v2.60.0 (#1279)
    https://github.com/alexfalkowski/go-service/pull/1707

--- internal/health/registrations.go
+++ internal/health/registrations.go
@@ -7,28 +7,18 @@ import (
 	hc "github.com/alexfalkowski/go-service/v2/health/checker"
 	"github.com/alexfalkowski/go-service/v2/time"
 	"github.com/linxGnu/mssqlx"
-	"go.uber.org/fx"
 )
 
-// Params for health.
-type Params struct {
-	fx.In
-
-	Health *Config
-	DB     *mssqlx.DBs
-}
-
-// NewRegistrations for health.
-func NewRegistrations(params Params) health.Registrations {
-	t := time.MustParseDuration(params.Health.Timeout)
-	d := time.MustParseDuration(params.Health.Duration)
+func registrations(db *mssqlx.DBs, cfg *Config) health.Registrations {
+	t := time.MustParseDuration(cfg.Timeout)
+	d := time.MustParseDuration(cfg.Duration)
 	registrations := health.Registrations{
 		server.NewRegistration("noop", d, checker.NewNoopChecker()),
 		server.NewOnlineRegistration(d, d),
 	}
 
-	if params.DB != nil {
-		registrations = append(registrations, server.NewRegistration("pg", d, hc.NewDBChecker(params.DB, t)))
+	if db != nil {
+		registrations = append(registrations, server.NewRegistration("pg", d, hc.NewDBChecker(db, t)))
 	}
 
 	return registrations
