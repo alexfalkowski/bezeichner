1f5e09de8df1eaccb267526645163f0f6961f838
Alex Falkowski <alexrfalkowski@gmail.com>
2025-06-23 07:30:25 +0200

    feat(generator): use rand.Generator

--- internal/generator/ulid.go
+++ internal/generator/ulid.go
@@ -1,20 +1,21 @@
 package generator
 
 import (
-	"crypto/rand"
-
 	"github.com/alexfalkowski/go-service/v2/context"
+	"github.com/alexfalkowski/go-service/v2/crypto/rand"
 	"github.com/alexfalkowski/go-service/v2/time"
 	"github.com/oklog/ulid"
 )
 
 // ULID generator.
-type ULID struct{}
+type ULID struct {
+	generator *rand.Generator
+}
 
 // Generate a ULID.
 func (k *ULID) Generate(_ context.Context, app *Application) (string, error) {
 	ms := ulid.Timestamp(time.Now())
-	id, err := ulid.New(ms, rand.Reader)
+	id, err := ulid.New(ms, k.generator)
 
 	return app.ID(id.String()), err
 }
