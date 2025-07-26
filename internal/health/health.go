136d636dcd021941e02c4a15a066948dec04083d
Alex Falkowski <alexrfalkowski@gmail.com>
2025-07-26 22:10:53 +0200

    feat(health): use just noop for grpc

--- internal/health/health.go
+++ internal/health/health.go
@@ -4,6 +4,7 @@ import (
 	v1 "github.com/alexfalkowski/bezeichner/api/bezeichner/v1"
 	"github.com/alexfalkowski/go-health/v2/checker"
 	"github.com/alexfalkowski/go-health/v2/server"
+	"github.com/alexfalkowski/go-service/v2/di"
 	"github.com/alexfalkowski/go-service/v2/env"
 	"github.com/alexfalkowski/go-service/v2/health"
 	hc "github.com/alexfalkowski/go-service/v2/health/checker"
@@ -11,20 +12,30 @@ import (
 	"github.com/linxGnu/mssqlx"
 )
 
-func register(name env.Name, srv *server.Server, db *mssqlx.DBs, cfg *Config) {
-	t := time.MustParseDuration(cfg.Timeout)
-	d := time.MustParseDuration(cfg.Duration)
+// RegisterParams for health.
+type RegisterParams struct {
+	di.In
+	Name   env.Name
+	Server *server.Server
+	DB     *mssqlx.DBs
+	Config *Config
+}
+
+// Register for health.
+func Register(params RegisterParams) {
+	t := time.MustParseDuration(params.Config.Timeout)
+	d := time.MustParseDuration(params.Config.Duration)
 	regs := health.Registrations{
 		server.NewRegistration("noop", d, checker.NewNoopChecker()),
 		server.NewOnlineRegistration(d, d),
 	}
 
-	if db != nil {
-		regs = append(regs, server.NewRegistration("pg", d, hc.NewDBChecker(db, t)))
+	if params.DB != nil {
+		regs = append(regs, server.NewRegistration("pg", d, hc.NewDBChecker(params.DB, t)))
 	}
 
-	srv.Register(name.String(), regs...)
-	srv.Register(v1.Service_ServiceDesc.ServiceName, regs...)
+	params.Server.Register(params.Name.String(), regs...)
+	params.Server.Register(v1.Service_ServiceDesc.ServiceName, regs[0])
 }
 
 func httpHealthObserver(name env.Name, server *server.Server) error {
