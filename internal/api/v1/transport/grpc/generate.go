36a581921434cb5dc245ea16400148d9c4c9c13f
Alex Falkowski <alexrfalkowski@gmail.com>
2025-11-24 19:14:12 +0100

    feat(strings): use strings.Empty

--- internal/api/v1/transport/grpc/generate.go
+++ internal/api/v1/transport/grpc/generate.go
@@ -4,6 +4,7 @@ import (
 	v1 "github.com/alexfalkowski/bezeichner/api/bezeichner/v1"
 	"github.com/alexfalkowski/go-service/v2/context"
 	"github.com/alexfalkowski/go-service/v2/meta"
+	"github.com/alexfalkowski/go-service/v2/strings"
 )
 
 // GenerateIdentifiers for gRPC.
@@ -11,7 +12,7 @@ func (s *Server) GenerateIdentifiers(ctx context.Context, req *v1.GenerateIdenti
 	resp := &v1.GenerateIdentifiersResponse{}
 	ids, err := s.id.Generate(ctx, req.GetApplication(), req.GetCount())
 
-	resp.Meta = meta.CamelStrings(ctx, "")
+	resp.Meta = meta.CamelStrings(ctx, strings.Empty)
 	resp.Ids = ids
 
 	return resp, s.error(err)
--- internal/api/v1/transport/grpc/map.go
+++ internal/api/v1/transport/grpc/map.go
@@ -4,6 +4,7 @@ import (
 	v1 "github.com/alexfalkowski/bezeichner/api/bezeichner/v1"
 	"github.com/alexfalkowski/go-service/v2/context"
 	"github.com/alexfalkowski/go-service/v2/meta"
+	"github.com/alexfalkowski/go-service/v2/strings"
 )
 
 // MapIdentifiers for gRPC.
@@ -11,7 +12,7 @@ func (s *Server) MapIdentifiers(ctx context.Context, req *v1.MapIdentifiersReque
 	resp := &v1.MapIdentifiersResponse{}
 	ids, err := s.id.Map(req.GetIds())
 
-	resp.Meta = meta.CamelStrings(ctx, "")
+	resp.Meta = meta.CamelStrings(ctx, strings.Empty)
 	resp.Ids = ids
 
 	return resp, s.error(err)
