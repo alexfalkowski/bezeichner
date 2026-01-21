fc71d4175980673f3899bf7a498b67b6ab976eba
Alejandro Falkowski <alexrfalkowski@gmail.com>
2026-01-21 19:03:31 +0100

    feat(generator): do not generate fake ids on error (#1585)

--- internal/generator/ksuid.go
+++ internal/generator/ksuid.go
@@ -2,6 +2,7 @@ package generator
 
 import (
 	"github.com/alexfalkowski/go-service/v2/context"
+	"github.com/alexfalkowski/go-service/v2/strings"
 	"github.com/segmentio/ksuid"
 )
 
@@ -11,6 +12,9 @@ type KSUID struct{}
 // Generate a KSUID.
 func (k *KSUID) Generate(_ context.Context, app *Application) (string, error) {
 	id, err := ksuid.NewRandom()
+	if err != nil {
+		return strings.Empty, err
+	}
 
-	return app.ID(id.String()), err
+	return app.ID(id.String()), nil
 }
--- internal/generator/nanoid.go
+++ internal/generator/nanoid.go
@@ -2,6 +2,7 @@ package generator
 
 import (
 	"github.com/alexfalkowski/go-service/v2/context"
+	"github.com/alexfalkowski/go-service/v2/strings"
 	nanoid "github.com/matoous/go-nanoid"
 )
 
@@ -11,6 +12,9 @@ type NanoID struct{}
 // Generate a NanoID.
 func (n *NanoID) Generate(_ context.Context, app *Application) (string, error) {
 	id, err := nanoid.Nanoid()
+	if err != nil {
+		return strings.Empty, err
+	}
 
-	return app.ID(id), err
+	return app.ID(id), nil
 }
