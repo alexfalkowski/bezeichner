43eb9bcc87d956e45aa1faff7772a40cb9691cc9
Alejandro Falkowski <alexrfalkowski@gmail.com>
2026-07-01 15:37:21 +0200

    test(deps): updated ruby dependencies (#2125)
    https://github.com/alexfalkowski/nonnative/releases/tag/v3.13.0

--- test/features/step_definitions/health/transport/grpc/observability_steps.rb
+++ test/features/step_definitions/health/transport/grpc/observability_steps.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 When('the system requests the health status with gRPC') do
-  request = Grpc::Health::V1::HealthCheckRequest.new(service: 'bezeichner.v1.Service')
-  @response = Bezeichner.health_grpc.check(request, Bezeichner.grpc_options)
+  @response = Bezeichner.health_grpc.check(deadline: Time.now + 10)
 end
 
 Then('the system should respond with a healthy status with gRPC') do
